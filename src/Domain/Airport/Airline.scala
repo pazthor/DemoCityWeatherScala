a6070d292383693f95ef382983283e2f7e62e492
Julio César Pastor Marcelo <final.africae@gmail.com>
2019-02-17 21:35:08 -0600

    Remove class.

--- src/Domain/Airport/Airline.scala
+++
@@ -1,9 +0,0 @@
-package Airport
-
-import scala.collection.mutable
-
-class Airline(airline: String) {
-  val AirLine:String  = airline
-  private val Flights: mutable.Map[String, String] =  collection.mutable.Map[String, String]()// TODO: debe ser lista o diccionario?
-
-}
