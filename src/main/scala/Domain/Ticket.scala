7800b8060a26a8eaee6341a8c361c9f3700ff9a6
Julio César Pastor Marcelo <final.africae@gmail.com>
2019-02-18 14:44:07 -0600

    add properties to Ticket.

--- src/main/scala/Domain/Ticket.scala
+++ src/main/scala/Domain/Ticket.scala
@@ -1,5 +1,9 @@
 package Domain
 
 class Ticket(airLineName: String, flightNum: String, originAirport:AirPort, destinyAirport: AirPort ) {
+  val AirLineName: String= airLineName
+  val FlightNum: String= flightNum
+  val OriginAirport:AirPort= originAirport
+  val DestinyAirport: AirPort= destinyAirport
 
 }
